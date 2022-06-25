361fd00de12007edd9037a45d273ba002e5fd9b6
Paul Morrow <pmorow93@gmail.com>
2022-06-25 11:29:10 -0500

    All tests passing

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -4,14 +4,91 @@ const express = require("express");
 const router = express.Router();
 const Actions = require("./actions-model");
 
-router.get("/api/actions", (req, res) => {});
+router.get("/", (req, res) => {
+  Actions.get()
+    .then((action) => {
+      res.json(action);
+    })
+    .catch(() => {
+      res.status(404).json({ message: "Could not retrieve the action" });
+    });
+});
 
-router.get("/api/actions/:id", (req, res) => {});
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  Actions.get(id)
+    .then((action) => {
+      if (action) {
+        res.json(action);
+      } else {
+        res
+          .status(404)
+          .json({ message: `action with id ${id} does not exist` });
+      }
+    })
+    .catch(() => {
+      res.status(404).json({ message: `Error fetching action with id: ${id}` });
+    });
+});
 
-router.post("/api/actions", (req, res) => {});
+router.post("/", (req, res) => {
+  const newAction = req.body;
+  if (
+    newAction.hasOwnProperty("notes") &&
+    newAction.hasOwnProperty("description") &&
+    newAction.hasOwnProperty("project_id")
+  ) {
+    Actions.insert(newAction)
+      .then((newAction) => {
+        res.json(newAction);
+      })
+      .catch(() => {
+        res.status(404).json({ message: `Failed to create new action` });
+      });
+  } else {
+    res
+      .status(400)
+      .json({ message: `Missing notes, description, and project_id` });
+  }
+});
 
-router.put("/api/actions/:id", (req, res) => {});
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const newAction = req.body;
+  if (
+    newAction.hasOwnProperty("notes") &&
+    newAction.hasOwnProperty("description") &&
+    newAction.hasOwnProperty("project_id")
+  ) {
+    Actions.update(id, newAction)
+      .then((action) => {
+        res.json(action);
+      })
+      .catch(() => {
+        res.status(404).json({ message: `Error updating the action` });
+      });
+  } else {
+    res.status(400).json({ message: `please enter required fields` });
+  }
+});
 
-router.delete("/api/actions/:id", (req, res) => {});
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  Actions.remove(id)
+    .then((wasActionDeleted) => {
+      if (wasActionDeleted) {
+        res.json(wasActionDeleted);
+      } else {
+        res
+          .status(404)
+          .json({ message: `could not find action with id: ${id}` });
+      }
+    })
+    .catch(() => {
+      res
+        .status(404)
+        .json({ message: `could not delete action with id: ${id}` });
+    });
+});
 
 module.exports = router;
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,8 +1,7 @@
 // Write your "projects" router here!
-const Projects = require("./projects-model");
-
 const express = require("express");
 const router = express.Router();
+const Projects = require("./projects-model");
 
 router.get("/", (req, res) => {
   Projects.get()
@@ -10,26 +9,103 @@ router.get("/", (req, res) => {
       res.json(project);
     })
     .catch((err) => {
-      res.json({ message: err.message });
+      res.status(404).json({ message: err.message });
     });
 });
 
 router.get("/:id", (req, res) => {
-  const project = req.params.id;
-  if (project) {
-    res.json(project);
-  }
-  res.status(404).json({
-    message: `project with id does not exist`,
-  });
+  const { id } = req.params;
+  Projects.get(id)
+    .then((project) => {
+      if (project) {
+        res.json(project);
+      } else {
+        res.status(404).json({ message: `No project with id: ${id} found` });
+      }
+    })
+    .catch(() => {
+      res
+        .status(404)
+        .json({ message: `Error fetching project with id: ${id}` });
+    });
 });
 
-router.post("/api/projects", (req, res) => {});
+router.post("/", (req, res) => {
+  const projectObj = req.body;
+  if (projectObj) {
+    if (
+      projectObj.hasOwnProperty("name") &&
+      projectObj.hasOwnProperty("description")
+    ) {
+      Projects.insert(projectObj)
+        .then((project) => {
+          res.json(project);
+        })
+        .catch(() => {
+          res.status(400).json({ message: "failed to create new project" });
+        });
+    } else {
+      res
+        .status(400)
+        .json({ message: "'Name' & 'Description' fields are required" });
+    }
+  } else {
+    res.status(400).json({ message: "missing name and description" });
+  }
+});
 
-router.put("/api/projects/:id", (req, res) => {});
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const projectObj = req.body;
+  if (projectObj) {
+    if (
+      projectObj.hasOwnProperty("name") &&
+      projectObj.hasOwnProperty("description") &&
+      projectObj.hasOwnProperty("completed")
+    ) {
+      Projects.update(id, projectObj)
+        .then((project) => {
+          res.json(project);
+        })
+        .catch(() => {
+          res.status(400).json({ message: "failed to update project" });
+        });
+    } else {
+      res.status(400).json({
+        message: "'Name' & 'Description' & 'Completed' fields are required",
+      });
+    }
+  } else {
+    res.status(400).json({ message: "send me yo data homie" });
+  }
+});
 
-router.delete("/api/projects/:id", (req, res) => {});
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  Projects.remove(id)
+    .then((wasProjectDeleted) => {
+      if (wasProjectDeleted) {
+        res.json(wasProjectDeleted);
+      } else {
+        res.status(404).json({ message: `No project with id: ${id} found` });
+      }
+    })
+    .catch(() => {
+      res
+        .status(404)
+        .json({ message: `Error deleting project with id: ${id}` });
+    });
+});
 
-router.get("/api/projects/:id/actions", (req, res) => {});
+router.get("/:id/actions", (req, res) => {
+  const { id } = req.params;
+  Projects.getProjectActions(id)
+    .then((project) => {
+      res.json(project);
+    })
+    .catch(() => {
+      res.status(404).json({ message: "Failed to fetch actions for id" });
+    });
+});
 
 module.exports = router;
